5a1034e4b59aaff12a25ee8cdc068ada95a655b8
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-02-13 15:30:37 -0800

    changed student controller

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -3,25 +3,25 @@ const mongoose = require('mongoose');
 
 // Get all students
 const getStudents = async (req, res) => {
-  const { classid } = req.params
+  const { class_id } = req.params
 
-  const students = await Student.find({ classId: classid }).sort({createdAt: -1})
+  const students = await Student.find({ class_Id: class_id }).sort({createdAt: -1})
 
   res.status(200).json(students)
 }
 
 // Get a single student
 const getStudent = async (req, res) => {
-  const { classid, id } = req.params
+  const { class_id, id } = req.params
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(404).json({ error: 'No such student' })
   }
 
-  const student = await Student.findOne({ classId: classid, _id: id })
+  const student = await Student.findOne({ class_Id: class_id, _id: id })
 
   if (!student) {
-      return res.status(404).json({ error: 'No such student' })
+      return res.status(404).json({ error: 'No no such student' })
   }
 
   res.status(200).json(student)
