8721b3c286ec235fe4869de41379e09427df9212
Danika Enad <danika.enad@gmail.com>
2023-02-26 18:02:07 -0800

    preliminary registerUser function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,27 @@
 const User = require('../models/userModel');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
+const asyncHandler = require('express-async-handler');
+
+//Create a new user
+const registerUser = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body
+
+  if(!firstName || !lastName || !email || !password) {
+   return res.status(400).json({error: 'Please input all fields'})
+  }
+
+  res.json({message: 'Register User'})
+}
+
+//Authenticate user
+const loginUser = asyncHandler(async (req, res) => {
+  res.json({message: 'Login User'})
+})
 
 // Get user
-const getUser = async (req, res) => {
+const getUser = asyncHandler(async (req, res) => {
   const { id } = req.params
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -22,8 +41,10 @@ const getUser = async (req, res) => {
     email: user.email,
     password: user.password
   })
-}
+})
 
 module.exports = {
-  getUser
+  getUser,
+  loginUser,
+  registerUser
 }
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,20 @@
 const express = require('express')
 
 const {
+  registerUser,
+  loginUser,
   getUser
 } = require('../controllers/userController')
 
 const router = express.Router()
 
+//Create a new user
+router.post('/', registerUser)
+
+//Autenticate a new user
+router.post('/login', loginUser)
+
 // Get single user
-router.get('/:id', getUser);
+router.get('/:id', getUser)
 
 module.exports = router
