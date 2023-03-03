b1f71e72628a3d61f03b71d9ddf52e33a154672d
Danika Enad <danika.enad@gmail.com>
2023-03-02 23:02:17 -0800

    removed process.env

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -8,7 +8,7 @@ const protect = async (req, res, next) => {
         try {
             token = req.headers.authorization.split(' ')[1]
 
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            const decoded = jwt.verify(token, 'test1234')
 
             req.user = await User.findById(decoded.id).select('-password')
 
